146d7a6f8b3e2313050061ed0f91fb4b3310c208
Nguyễn Tạ Huy Hoàng <85854007+NguyenTaHuyHoang@users.noreply.github.com>
2024-06-26 16:39:33 +0700

    Update EthTransfer.sol

--- Smart contract/EthTransfer.sol
+++ Smart contract/EthTransfer.sol
@@ -1,10 +1,22 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.8.2 <0.9.0;
 
 contract EthTransfer {
+    address public owner;
+
+    // Hàm khởi tạo hợp đồng, lưu địa chỉ của người triển khai
+    constructor() {
+        owner = msg.sender;
+    }
+
     // Hàm để gửi ETH
     function sendEth(address payable recipient) public payable {
         require(msg.value > 0, "Must send some ETH");
-        require(address(this).balance > msg.value );
         recipient.transfer(msg.value);
+    }
+
+    // Hàm để lấy số dư của người triển khai hợp đồng
+    function getOwnerBalance() public view returns (uint256){
+        return owner.balance;
     }
 }
